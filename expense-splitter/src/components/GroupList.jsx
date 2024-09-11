f2c3625eaf030de7c5064c92b03aa03071be372c
belunatic <abelluvyou@yahoo.com>
2024-09-11 13:56:00 -0700

    Made some modification to the Button Component, Edit Groups Components

--- expense-splitter/src/components/GroupList.jsx
+++ expense-splitter/src/components/GroupList.jsx
@@ -1,43 +1,50 @@
-import { useState } from 'react'
-import Button from './Button'
+import { useState } from "react";
+import Button from "./Button";
 
-export default function GroupList({ groupData }) {
-  const [displayDetails, setDisplayDetails] = useState('')
+export default function GroupList({ groupData, setGroupData }) {
+	const [displayDetails, setDisplayDetails] = useState("");
 
-  const handleDisplayDetails = (id) => {
-    console.log(id)
-    if (displayDetails === id) {
-      setDisplayDetails('')
-    } else {
-      setDisplayDetails(id)
-    }
-  }
+	const handleDisplayDetails = (id) => {
+		console.log(id);
+		if (displayDetails === id) {
+			setDisplayDetails("");
+		} else {
+			setDisplayDetails(id);
+		}
+	};
 
-  const groupList = groupData.map((group) => (
-    <div
-      onClick={() => handleDisplayDetails(group.id)}
-      key={group.id}
-      className="flex flex-col bg-slate-100 rounded-md py-4 px-4 mb-1 cursor-pointer"
-    >
-      <div className="flex justify-between">
-        <div className="text-lg">{group.name}</div>
-        <div className="flex gap-2">
-          <Button variant={'small'}>Edit</Button>
-          <Button variant={'small'}>Delete</Button>
-        </div>
-      </div>
-      <div>
-        {displayDetails === group.id ? (
-          <div className="text-sm font-light font-roboto">
-            <p>Description: {group.description}</p>
-            <p> Budget: {group.budget}</p>
-          </div>
-        ) : (
-          ''
-        )}
-      </div>
-    </div>
-  ))
+	const handleDelete = (id) => {
+		console.log("hello");
+		const updatedGroupData = groupData.filter((item) => item.id !== id);
+		setGroupData(updatedGroupData);
+	};
 
-  return <div className="mb-4">{groupList}</div>
+	const groupList = groupData.map((group) => (
+		<div
+			onClick={() => handleDisplayDetails(group.id)}
+			key={group.id}
+			className="flex flex-col bg-slate-100 rounded-md py-4 px-4 mb-1 cursor-pointer">
+			<div className="flex justify-between">
+				<div className="text-lg">{group.name}</div>
+				<div className="flex gap-2">
+					<Button variant={"small"}>Edit</Button>
+					<Button variant={"small"} onClick={() => handleDelete(group.id)}>
+						Delete
+					</Button>
+				</div>
+			</div>
+			<div>
+				{displayDetails === group.id ? (
+					<div className="text-sm font-light font-roboto">
+						<p>Description: {group.description}</p>
+						<p> Budget: {group.budget}</p>
+					</div>
+				) : (
+					""
+				)}
+			</div>
+		</div>
+	));
+
+	return <div className="mb-4">{groupList}</div>;
 }
