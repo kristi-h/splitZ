47482f644d7df080607c765fce8a7e4b28c31848
belunatic <abelluvyou@yahoo.com>
2024-09-12 19:13:28 -0700

    MultiSelectionDropdown Added, Friends sample data in SiteContext

--- expense-splitter/src/components/context/SiteContext.jsx
+++ expense-splitter/src/components/context/SiteContext.jsx
@@ -1,23 +1,45 @@
-import React, {useState} from 'react'
-import { nanoid } from 'nanoid'
-import { createContext, useContext } from 'react'
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import { createContext, useContext } from "react";
 
-const SiteContext = createContext(null)
+const SiteContext = createContext(null);
 
-export const UseDataContext = () => useContext(SiteContext)
+export const UseDataContext = () => useContext(SiteContext);
 
-export const DataProvider = ({children}) => {
-    const [expense, setExpense] = useState([])
+export const DataProvider = ({ children }) => {
+	const [expense, setExpense] = useState([]);
 
-    const handleSetExpense = (values) => {
-        setExpense(prev => [...prev, { ...values, id: nanoid(), date: new Date() }])
-    }
-    console.log(expense)
+	const handleSetExpense = (values) => {
+		setExpense((prev) => [
+			...prev,
+			{ ...values, id: nanoid(), date: new Date() },
+		]);
+	};
 
+	//participation / friends
 
-    return (
-        <SiteContext.Provider value={{ expense, setExpense, handleSetExpense }}>
-            {children}
-        </SiteContext.Provider>
-    )
-}
+	const [friends, setFriends] = useState([
+		{
+			id: 1,
+			name: "Abel",
+			email: "abel@hotlikehell.com",
+		},
+		{
+			id: 2,
+			name: "Kristin",
+			email: "kristin@hotlikehell.com",
+		},
+		{
+			id: 3,
+			name: "Carlos",
+			email: "carlos@hotlikehell.com",
+		},
+	]);
+
+	return (
+		<SiteContext.Provider
+			value={{ expense, setExpense, handleSetExpense, friends }}>
+			{children}
+		</SiteContext.Provider>
+	);
+};
---
+++ expense-splitter/src/components/ui/MultiSelectDropdown.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+
+export default function MultiSelectDropdown({ options, control }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	// Function to toggle dropdown
+	const toggleDropdown = ({ options, control }) => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<div className="relative">
+			<div
+				className="border p-2 cursor-pointer border p-2 cursor-pointer text-sm font-light bg-gray-200 rounded-md"
+				onClick={toggleDropdown}
+				onFocusOut={toggleDropdown}>
+				Select Friends
+			</div>
+			{isOpen && (
+				<div className="absolute mt-1 border bg-white z-10 max-h-60 overflow-y-auto bg-gray-200">
+					{options.map((friend) => (
+						<label key={friend.id} className="flex items-center p-2">
+							<Controller
+								name="friends"
+								control={control}
+								render={({ field }) => (
+									<input
+										type="checkbox"
+										value={friend.id}
+										onChange={(e) => {
+											const isChecked = e.target.checked;
+											const valueArray = field.value || [];
+											const newValue = isChecked
+												? [...valueArray, friend.id]
+												: valueArray.filter((id) => id !== friend.id);
+											field.onChange(newValue);
+										}}
+										checked={field.value?.includes(friend.id) || false}
+									/>
+								)}
+							/>
+							<span className="ml-2">{friend.name}</span>
+						</label>
+					))}
+				</div>
+			)}
+		</div>
+	);
+}
