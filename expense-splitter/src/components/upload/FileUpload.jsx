7604aee84ea457beb2858405ba3c1f430979a899
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-09-14 19:55:28 -0700

    Get url from uploaded file

--- expense-splitter/src/components/upload/FileUpload.jsx
+++ expense-splitter/src/components/upload/FileUpload.jsx
@@ -2,7 +2,8 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { storage } from "../../utils/firebase";
-import { ref, uploadBytes } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useState } from "react";
 
 // Define validation schema
 const schema = z.object({
@@ -31,6 +32,9 @@ const schema = z.object({
 
 // Grab objects from useForm hook
 const FileUpload = () => {
+  // Store url of uploaded image
+  const [imageUrl, setImageUrl] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -45,34 +49,49 @@ const FileUpload = () => {
     // console.log(typeof data.upload[0].lastModifiedDate);
 
     // Define where to store with filepath
+    // TODO - Add unique identifier to filename //
     const uploadRef = ref(storage, data.upload[0].name);
     // Upload to storage
-    await uploadBytes(uploadRef, data.upload[0]);
-    alert("File Uploaded");
+    const snapshot = await uploadBytes(uploadRef, data.upload[0]);
+    // Get url
+    const url = await getDownloadURL(snapshot.ref);
+    setImageUrl(url);
     reset();
   };
 
   return (
-    <form className="flex flex-col" onSubmit={handleSubmit(onUpload)}>
-      <h1>Upload Receipt</h1>
-      <div className="flex items-center gap-4">
-        <label htmlFor="upload">Upload File</label>
-        <p className="text-sm font-light">(File size must be less than 6MB)</p>
-      </div>
-      <input className="p-2" type="file" id="upload" {...register("upload")} />
-      {/* Conditionally render button based on isSubmitting */}
-      <button type="submit" disabled={isSubmitting}>
-        {isSubmitting ? "Uploading..." : "Upload"}
-      </button>
-      {/* Render size error */}
-      {errors.upload?.[0].size && (
-        <span className="text-red-500">{errors.upload[0].size.message}</span>
-      )}
-      {/* Render type error */}
-      {errors.upload?.[0].type && (
-        <span className="text-red-500">{errors.upload[0].type.message}</span>
-      )}
-    </form>
+    <>
+      <form className="flex flex-col" onSubmit={handleSubmit(onUpload)}>
+        <h1>Upload Receipt</h1>
+        <div className="flex items-center gap-4">
+          <label htmlFor="upload">Upload File</label>
+          <p className="text-sm font-light">
+            (File size must be less than 6MB)
+          </p>
+        </div>
+        <input
+          className="p-2"
+          type="file"
+          id="upload"
+          {...register("upload")}
+        />
+        {/* Conditionally render button based on isSubmitting */}
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Uploading..." : "Upload"}
+        </button>
+        {/* Render size error */}
+        {errors.upload?.[0].size && (
+          <span className="text-red-500">{errors.upload[0].size.message}</span>
+        )}
+        {/* Render type error */}
+        {errors.upload?.[0].type && (
+          <span className="text-red-500">{errors.upload[0].type.message}</span>
+        )}
+      </form>
+      {/* Display uploaded img NOT WORKING! */}
+      <img src={imageUrl} alt="" />
+      {imageUrl && <a href={imageUrl}>Download</a>}
+    </>
   );
 };
 
