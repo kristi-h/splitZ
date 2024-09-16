5629dbd081cede1f2033d6b0b57189982a383ac1
Carlos Cespedes <cespedes88@gmail.com>
2024-09-15 21:39:41 -0700

    Adjust button padding defaults

--- expense-splitter/src/components/ui/Button.jsx
+++ expense-splitter/src/components/ui/Button.jsx
@@ -1,8 +1,8 @@
 export default function Button({ children, variant, onClick, className }) {
   const buttonStyles =
     variant === 'small'
-      ? 'bg-accent text-white py-1 px-[0.7rem] rounded-[0.25rem] hover:bg-secondary text-sm font-light'
-      : 'bg-accent text-white py-1 px-5 rounded-md hover:bg-secondary'
+      ? 'bg-accent text-white py-2 px-6 rounded-[0.25rem] hover:bg-secondary text-sm font-light'
+      : 'bg-accent text-white py-2 px-5 rounded-md hover:bg-secondary'
 
   return (
     <button
