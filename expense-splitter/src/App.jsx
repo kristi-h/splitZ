2987160e1384569b72f146019d89e641f1cd2b50
Matthew Neie <matneie@gmail.com>
2024-09-17 10:00:10 -0700

    add react-router-dom with links to endpoints, work on dropdown icon

--- expense-splitter/src/App.jsx
+++ expense-splitter/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Expense from './components/expense/Expense'
 import Group from './components/group/Group'
 import Footer from './components/layout/Footer'
@@ -7,19 +8,25 @@ import Modal from './components/ui/Modal'
 
 function App() {
   return (
-    <DataProvider>
-      <Modal />
-      <div className='flex flex-col h-[100vh]'>
-        <Header />
-        <div className='flex flex-col h-full overflow-y-auto'>
-          <section className="font-semibold px-4 py-6">
-          <Group />
-          <Expense />
-          </section>
-          <Footer/>
+    <Router>
+      <DataProvider>
+        <Modal />
+        <div className='flex flex-col h-[100vh]'>
+          <Header />
+          <div className='flex flex-col h-full overflow-y-auto'>
+              <section className="font-semibold px-4 py-6">
+                <Routes>
+                  <Route path="/home" />
+                  <Route path="/groups" element={<Group />} />
+                  <Route path="/friends" />
+                  <Route path="/expenses" element={<Expense />} />
+                </Routes>
+              </section>
+            <Footer/>
+          </div>
         </div>
-      </div>
-    </DataProvider>
+      </DataProvider>
+    </Router>
   )
 }
 
--- expense-splitter/src/components/layout/Header.jsx
+++ expense-splitter/src/components/layout/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import IconButton from '../ui/IconButton'
 
 export default function Header() {
@@ -6,10 +7,18 @@ export default function Header() {
       <div className='text-center text-[40px] uppercase font-extrabold'>Let's Split It</div>
       <nav className='mt-auto mx-4'>
         <div className='flex justify-between gap-2'>
-          <IconButton icon={'house'}>Home</IconButton>
-          <IconButton icon={'people-group'}>Groups</IconButton>
-          <IconButton icon={'address-book'}>Friends</IconButton>
-          <IconButton icon={'credit-card'}>Expenses</IconButton>
+            <Link to="/home">
+              <IconButton icon={'house'}>Home</IconButton>
+            </Link>
+            <Link to="/groups">
+              <IconButton icon={'people-group'}>Groups</IconButton>
+            </Link>
+            <Link to="/friends">
+              <IconButton icon={'address-book'}>Friends</IconButton>
+            </Link>
+            <Link to="/expenses">
+              <IconButton icon={'credit-card'}>Expenses</IconButton>
+            </Link>
         </div>
       </nav>
     </div>
