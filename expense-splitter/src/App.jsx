108c9853ef35475a0e4165bd5696a855d447ef7d
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-09-17 13:35:16 -0700

    Rename component to use friend terminology

--- expense-splitter/src/App.jsx
+++ expense-splitter/src/App.jsx
@@ -1,40 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Expense from './components/expense/Expense'
-import Group from './components/group/Group'
-import Footer from './components/layout/Footer'
-import { DataProvider } from './components/context/SiteContext'
-import Header from './components/layout/Header'
-import Modal from './components/ui/Modal'
-import CreateParticipant from './components/CreateParticipant'
-import CreateExpense from './components/expense/CreateExpense'
-import ReceiptUpload from './components/upload/ReceiptUpload'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Expense from "./components/expense/Expense";
+import Group from "./components/group/Group";
+import Footer from "./components/layout/Footer";
+import { DataProvider } from "./components/context/SiteContext";
+import Header from "./components/layout/Header";
+import Modal from "./components/ui/Modal";
+import CreateExpense from "./components/expense/CreateExpense";
+import ReceiptUpload from "./components/upload/ReceiptUpload";
 
 function App() {
   return (
     <Router>
       <DataProvider>
-        <div className='flex flex-col h-[100vh]'>
+        <div className="flex h-[100vh] flex-col">
           <Header />
-          <div className='flex flex-col h-full overflow-y-auto'>
-              <Modal />
-              <section className="font-semibold px-4 py-6">
-                <Routes>
-                  <Route path="/home" />
-                  <Route path="/groups" element={<Group />} />
-                  <Route path="/friends" />
-                  <Route path="/expenses" element={<Expense />} />
-                  <Route path="/receipts" element={<ReceiptUpload />} />
-                    {/* <Expense /> */}
-                    {/* <CreateExpense /> */}
-                    {/* <CreateParticipant /> */}
-                </Routes>
-              </section>
-              <Footer/>
-           </div>
+          <div className="flex h-full flex-col overflow-y-auto">
+            <Modal />
+            <section className="px-4 py-6 font-semibold">
+              <Routes>
+                <Route path="/home" />
+                <Route path="/groups" element={<Group />} />
+                <Route path="/friends" />
+                <Route path="/expenses" element={<Expense />} />
+                <Route path="/receipts" element={<ReceiptUpload />} />
+                {/* <Expense /> */}
+                {/* <CreateExpense /> */}
+                {/* <CreateParticipant /> */}
+              </Routes>
+            </section>
+            <Footer />
+          </div>
         </div>
       </DataProvider>
     </Router>
-  )
+  );
 }
 
 export default App;
