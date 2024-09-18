0be21fe01fe017143aefe14f2ec5494228791bb4
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-09-17 19:54:08 -0700

    Update heading case

--- expense-splitter/src/friends/FriendsView.jsx
+++ expense-splitter/src/friends/FriendsView.jsx
@@ -8,7 +8,7 @@ const FriendsView = () => {
   return (
     !modal.show && (
       <>
-        <h1 className="text-center">FRIENDS</h1>
+        <h1 className="text-center">Friends</h1>
         <FriendsList />
         <Button
           className="over absolute bottom-6 left-1/2 z-10 h-14 w-[200px] -translate-x-1/2 rounded-md bg-primary text-lg"
