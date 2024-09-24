05f2d0479ba0dbc096691dc64b4252807e805ba3
Carlos Cespedes <cespedes88@gmail.com>
2024-09-23 22:56:45 -0700

    Limit group and expense display to three, handle non-existent data

--- expense-splitter/src/components/Home.jsx
+++ expense-splitter/src/components/Home.jsx
@@ -2,12 +2,11 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { UseDataContext } from "./context/SiteContext";
 import Card from "./ui/Card";
+import Button from "./ui/Button";
 
 export default function Home() {
   const navigate = useNavigate();
   const { user, groupData, expenses, modal } = UseDataContext();
-
-  console.log(groupData);
 
   useEffect(() => {
     // if user is not "logged in", go to login
@@ -16,32 +15,40 @@ export default function Home() {
     }
   }, [user]);
 
-  const groupDisplay = groupData.map((group) => (
-    <Card
-      key={group.id}
-      id={group.id}
-      icon={"fa-user-group"}
-      title={group.name}
-      subtitle={group.description}
-    />
-  ));
+  const groupDisplay = groupData.map((group, i) => {
+    // show a max of 3 groups
+    if (i <= 2) {
+      return (
+        <Card
+          key={group.id}
+          id={group.id}
+          icon={"fa-user-group"}
+          title={group.name}
+          subtitle={group.description}
+        />
+      );
+    }
+  });
 
-  const expenseDisplay = expenses.map((expense) => {
-    // get the group associated with this expense
-    const expenseGroup = groupData.find(
-      (group) => group.id === expense.groupId,
-    );
+  const expenseDisplay = expenses.map((expense, i) => {
+    // show a max of 3 expenses
+    if (i <= 2) {
+      // get the group associated with this expense
+      const expenseGroup = groupData.find(
+        (group) => group.id === expense.groupId,
+      );
 
-    return (
-      <Card
-        key={expense.id}
-        id={expense.id}
-        icon={"fa-money-check-dollar"}
-        title={expense.name}
-        subtitle={expenseGroup.name}
-        price={expense.amount}
-      />
-    );
+      return (
+        <Card
+          key={expense.id}
+          id={expense.id}
+          icon={"fa-money-check-dollar"}
+          title={expense.name}
+          subtitle={expenseGroup?.name}
+          price={expense.amount}
+        />
+      );
+    }
   });
 
   return (
@@ -51,12 +58,38 @@ export default function Home() {
         <h1 className="text-center">Welcome, {user}!</h1>
         <div className="mb-8">
           <h2 className="mb-4 text-2xl font-medium">Recent Groups</h2>
-          {groupDisplay}
+          {groupData.length > 0 ? (
+            <>{groupDisplay}</>
+          ) : (
+            <>
+              <div className="mb-2 flex cursor-pointer flex-col items-center justify-center rounded-2xl border border-dashed border-accent/50 p-4 py-12">
+                <p className="font-semibold">There are no groups to display</p>
+                <p className="text-sm">Get started by creating a group.</p>
+                <Button className="mt-4 w-full bg-primary md:w-auto">
+                  Create a Group
+                </Button>
+              </div>
+            </>
+          )}
         </div>
 
         <div className="mb-8">
           <h2 className="mb-4 text-2xl font-medium">Recent Expenses</h2>
-          {expenseDisplay}
+          {expenses.length > 0 ? (
+            <>{expenseDisplay}</>
+          ) : (
+            <>
+              <div className="mb-2 flex cursor-pointer flex-col items-center justify-center rounded-2xl border border-dashed border-accent/50 p-4 py-12">
+                <p className="font-semibold">
+                  There are no expenses to display
+                </p>
+                <p className="text-sm">Get started by creating an expense.</p>
+                <Button className="mt-4 w-full bg-primary md:w-auto">
+                  Create an Expense
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     )
--- expense-splitter/src/components/ui/Button.jsx
+++ expense-splitter/src/components/ui/Button.jsx
@@ -8,7 +8,7 @@ export default function Button({
   const buttonStyles =
     variant === "small"
       ? "bg-accent text-white py-2 px-6 rounded-md hover:bg-secondary text-sm font-light"
-      : "bg-accent text-white py-2 px-8 rounded-lg hover:bg-secondary";
+      : "bg-accent text-white py-3 max-w-[380px] px-8 rounded-lg hover:bg-secondary";
 
   return (
     <button
