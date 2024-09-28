67ebef93feaea8bd0ffb87cec5e060d8b030678c
Carlos Cespedes <cespedes88@gmail.com>
2024-09-27 21:15:14 -0700

    Adjust sizing of header and IconButton to accomodate smaller mobile devices

--- expense-splitter/src/components/ui/IconButton.jsx
+++ expense-splitter/src/components/ui/IconButton.jsx
@@ -4,10 +4,10 @@ export default function IconButton({ children, icon, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="text-slate mx-auto flex h-[64px] w-[96px] flex-col items-center rounded-lg py-[0.16rem] transition-all hover:bg-white hover:text-primary/90"
+      className="text-slate mx-auto flex h-[54px] w-[80px] flex-col items-center rounded-lg py-[0.16rem] transition-all hover:bg-white hover:text-primary/90"
     >
-      <i className={`fa-solid ${iconType} text-2xl`}></i>
-      <div className="text-[18px] font-semibold">{children}</div>
+      <i className={`fa-solid ${iconType} text-xl`}></i>
+      <div className="text-[14px] font-semibold">{children}</div>
     </button>
   );
 }
