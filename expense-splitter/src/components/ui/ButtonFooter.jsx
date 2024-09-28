369f6b92018bd924a8b4f06106aeed8eb2538ebe
Carlos Cespedes <cespedes88@gmail.com>
2024-09-28 14:05:53 -0700

    Add ButtonFooter component to house floating footer buttons

---
+++ expense-splitter/src/components/ui/ButtonFooter.jsx
@@ -0,0 +1,9 @@
+const ButtonFooter = ({ children }) => {
+  return (
+    <div className="over fixed bottom-0 left-1/2 z-10 flex w-full -translate-x-1/2 bg-gradient-to-t from-white to-white/0 pb-5 pt-20">
+      <div className="mx-auto flex gap-2">{children}</div>
+    </div>
+  );
+};
+
+export default ButtonFooter;
