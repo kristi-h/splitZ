777981838b94d47e12c3c97f1314bdc3ca9e6cfa
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-10-03 13:42:52 -0700

    Basic message for 404 page

--- expense-splitter/src/components/layout/NotFound.jsx
+++ expense-splitter/src/components/layout/NotFound.jsx
@@ -1,5 +1,16 @@
 const NotFound = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <>
+      <h1 className="text-center">Page Not Found</h1>
+      <p className="text-center">
+        The link you followed may be broken or the page may have been
+        removed.&nbsp;
+        <a href="/" className="text- text-primary">
+          <u>Go back to the home page.</u>
+        </a>
+      </p>
+    </>
+  );
 };
 
 export default NotFound;
