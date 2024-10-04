a421f6824508bf9d523069a7d3db343a7cf51f67
Matthew Neie <132638527+MatthewNeie@users.noreply.github.com>
2024-10-03 22:15:18 -0700

    Update CardExpenseDetail.jsx
    remove cursor-pointer

--- expense-splitter/src/components/ui/CardExpenseDetail.jsx
+++ expense-splitter/src/components/ui/CardExpenseDetail.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function Card({ id, type, icon, title, subtitle, price }) {
   return (
-    <div className="mb-2 flex cursor-pointer items-center rounded-2xl bg-card-bg p-4">
+    <div className="mb-2 flex items-center rounded-2xl bg-card-bg p-4">
       <div className="flex w-full items-center justify-between">
         <div className="flex items-center">
           <div className="mr-4 rounded-xl bg-primary p-3">
